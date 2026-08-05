926b38f9c7e61daef3176d7db3e47d82dea7a32d
oscaredu1226 <oscaredu01@hotmail.com>
2026-08-04 20:26:14 -0500

    feat(hero): add hero section component

---
+++ src/app/landing/components/hero-section/hero-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  templateUrl: './hero-section.component.html',
+  styleUrl: './hero-section.component.css'
+})
+export class HeroSectionComponent {}
