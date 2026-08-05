9cff08131687e6a2463440d00116d6c7c016cb0e
oscaredu1226 <oscaredu01@hotmail.com>
2026-08-04 20:59:17 -0500

    feat(whatsapp): add floating community button logic

---
+++ src/app/landing/components/whatsapp-float/whatsapp-float.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whatsapp-float',
+  templateUrl: './whatsapp-float.component.html',
+  styleUrl: './whatsapp-float.component.css'
+})
+export class WhatsappFloatComponent {
+  protected readonly communityUrl =
+    'https://chat.whatsapp.com/LePZo8MJara3wuqSfBa8cj';
+}
